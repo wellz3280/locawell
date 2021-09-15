a4109b4c52e6b153e134ff243c3bbd253205624b
welington <welington.a2you@gmail.com>
2021-09-15 17:22:16 -0300

    iclusão das tags link rel style.css e reset.css

---
+++ index.php
@@ -0,0 +1,12 @@
+<!Doctype html5>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Locawell</title>
+		<link rel="stylesheet" type="text/css" href="media/css/style.css">
+		<link rel="stylesheet" type="text/css" href="media/css/reset.css">
+	</head>
+	<body>
+	
+	</body>
+</html>
