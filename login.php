f18a3301f9ba135c0449660fa0ea2813ba8821f5
welington <welington.a2you@gmail.com>
2021-09-28 01:42:34 -0300

    criada pagina para login

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Locawell- well</title>
+	<link rel="stylesheet" type="text/css" href="media/css/reset.css">
+	<link rel="stylesheet" type="text/css" href="media/css/style.css">
+</head>
+
+<body style="background: url(media/img/logo.png);">
+	<section class="login">
+			
+		<form>
+			
+			<input class="input-login" type="text" name="login" id="login" placeholder="Retr0" required>
+			
+			<input class="input-login" type="password" name="pass" id="pass" placeholder="**************" required>
+		
+			<input class="enviar-login" type="submit" value="Entrar">
+		</form>
+
+
+	</section>
+</body>
+</html>
