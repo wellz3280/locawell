7925b4c6ab2a82150599605707a7d1c9cebdfa89
welington <welington.a2you@gmail.com>
2021-09-28 03:55:12 -0300

    usando include para usar codigo esterno

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
 		<header>
 			<div class="topo">
 				<h1>
-						<a href="index.php?pagina=index.php"><img src="media/img/logo.png" alt="logo"></a>
+						<a href="index.php?pag=0"><img src="media/img/logo.png" alt="logo"></a>
 				<nav>
 					<ul>
-						<li><a href="cadastrarCliente.html">cadastrar</a></li>
+						<li><a href="index.php?pag=1">cadastrar</a></li>
 						<li><a href="#">alugar</a></li>
 						<li><a href="#">devolver</a></li>
-						<li><a href="login.php?pagina=login.php">Login</a></li>
+						<li><a href="index.php?pag=3">Login</a></li>
 					</ul>
 				</nav>
 	
@@ -28,15 +28,9 @@
 		<main>
 			<?php
 
-				$pagina = $_GET['pagina'];
-				if ($pagina == 0) {
-					require_once('home.php');
-				}else{
-					require_once($pagina);
-				}
-
-
 				
+
+				include 'paginas.php';
 			?>
 
 		</main>
