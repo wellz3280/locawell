ed6698851307e08928492ea3d9c9e704bc9cc8ca
welington <welington.a2you@gmail.com>
2021-09-28 03:55:31 -0300

    troca de extenasão para .php

---
+++ cadastrarProdutos.php
@@ -0,0 +1,61 @@
+<!Doctype html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Locawell</title>
+		<link rel="stylesheet" type="text/css" href="media/css/style.css">
+		<link rel="stylesheet" type="text/css" href="media/css/reset.css">
+	</head>
+	<body>
+		
+
+			<section class="cadastro-cliente">	
+
+				<h2>
+					<ul >
+						<li><a href="index.php?pag=1">Clientes</a></li>
+						<li>Produtos</li>
+						<li><a href="cadastrarForncedor.html">Fornecedores</a></li>
+
+					</ul>
+					
+				
+				</h2>
+
+
+					<form>
+						<fieldset>
+							
+							
+							<input class="input-padrao" type="text" id="titulo" name="titulo" placeholder="Titulo do Filme" required>	
+
+							
+							<input class="input-padrao" type="text" id="subtitulo" name="subtitulo" placeholder="Subtitulo" required>
+
+							<input class="input-padrao" type="text" id="ano" name="ano" placeholder="Ano de Lançamento" required>
+
+							<input class="input-padrao" type="text" id="genero" name="genero" placeholder="Gênero" required>
+
+							<input class="input-padrao" type="text" id="duracao" name="duracao" placeholder="Duração" required >
+
+							<input class="input-padrao" type="text" id="diretor" name="diretor" placeholder="Diretor" required >
+							
+							<input class="input-padrao" type="text" id="ator-pricipal" name="ator-pricipal" placeholder="Ator Principal" required >
+
+							<input class="input-padrao" type="text" id="ator-coadjuvante" name="ator-coadjuvante" placeholder="Ator Coadjuvante" required >
+
+							<input class="input-padrao" type="text" id="pais-origem" name="pais-origem" placeholder="País de Origem" required >
+
+							<input class="enviar" type="submit" value="Cadastrar" name="">
+						</fieldset>
+
+					</form>
+
+
+			</section>
+
+		</main>
+		
+		
+	</body>
+</html>
