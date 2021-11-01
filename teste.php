2eff38e05a5f540d14ef45541fc2d5c5ea8cc362
weliton <welingtonzsilva@gmail.com>
2021-11-01 19:25:12 -0300

    realizando testes

--- teste.php
+++ teste.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 use Weliton\Locawell\Domain\Model\Ator;
 use Weliton\Locawell\Domain\Model\Diretor;
@@ -13,6 +14,25 @@ $mysql = Connection::Conecta();
 
 $teste = new RepositoryFilmes($mysql);
 
+
+
+$novoFilme = new Filme(
+    intval($_POST['idFilme']),
+    $_POST['titulo'],
+    $_POST['subtitulo'],
+    $_POST['ano'],
+    $_POST['duracao'],
+    intval($_POST['idGenero']),
+    intval($_POST['idAtor']),
+    intval($_POST['idDiretor'])
+);
+
+
+
+var_dump($novoFilme);
+$teste->insereBanco($novoFilme);
+
+exit();
 $tabela = 'filmes';
 
 foreach($teste->exibirConteudoBanco($tabela) as $listas){
@@ -25,11 +45,14 @@ foreach($teste->exibirConteudoBanco($tabela) as $listas){
 }
 
 
-$diretor = new Diretor(1,'whashinton');
+$diretor = new Diretor(4,'whashinton');
 $genero = new Genero(5,'terror');
 $ator = new Ator(4,'weliton');
-$filme = new Filme(5,'gato escaldado no fim do relacionamento','cachorro que fez','1995','1h30',
+$filme = new Filme(null,'Malditos espelhos ','sou feião mano','1995','1h30',
 $genero->idGenero(),$ator->idAtor(),$diretor->idDiretor());
 
-
+echo "#diretor:".$diretor->idDiretor();
+echo "<pre>";
+var_dump($filme);
+echo "</pre>";
 $teste->insereBanco($filme);
