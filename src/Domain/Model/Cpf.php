b6a8933602f42772ef6b66d5cf958e50f6c5a229
weliton <welingtonzsilva@gmail.com>
2021-11-03 03:12:16 -0300

    classe cpf criada e metodo do validação por expressoes regulares implementado

---
+++ src/Domain/Model/Cpf.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace Weliton\Locawell\Domain\Model;
+
+class Cpf
+{
+    private string $cpf;
+
+    public function __construct(string $cpf)
+    {
+        
+        if($this->validaCpf($cpf)){
+            $this->setCpf($cpf);
+        }else{
+            $this->setCpf('Cpf Invalido tente xxx.xxx.xxx-xx');
+        }
+    }
+
+    private function validaCpf(string $cpf):int
+    {
+        return preg_match('/^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$/',$cpf,$encontrados);
+    }
+
+    private function setCpf(string $cpf):void
+    {
+        $this->cpf = $cpf;
+    }
+}
