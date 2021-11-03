6b1fdd9376b36df8b3e4ce9c6c14194e8057ab21
weliton <welingtonzsilva@gmail.com>
2021-11-03 03:25:07 -0300

    classe modelo implementada

--- src/Domain/Model/Pessoa.php
+++ src/Domain/Model/Pessoa.php
@@ -4,6 +4,49 @@
 class Pessoa 
 {
     private string $nome;
-    private string 
+    private string $sobrenome;
+    private  $cpf;
+    private string $dataNasc;
+    private  $dependente;
+    private  $endereco;
+    private  $email;
+    private  $telefone;
+
+    public function __construct(string $nome, string $sobrenome,
+     Cpf $cpf, string $dataNasc, Dependente $dependente, Endereco $endereco, Email $email, Telefone $telefone )
+    {
+        $this->nome = $nome;
+        $this->sobrenome = $sobrenome;
+        $this->cpf = $cpf;
+        $this->dataNasc = $dataNasc;
+        $this->dependente = $dependente; 
+        $this->endereco = $endereco;
+        $this->email = $email;
+        $this->telefone = $telefone;
+    }
+
+    public function getNome():string
+    {   
+        return $this->nome;
+    }
+
+    public function getSobrenome():string
+    {   
+        return $this->sobrenome;
+    }
+
+    public function getDataNasc():string
+    {   
+        return $this->dataNasc;
+    }
+
+    private function aniversario(string $dataNasc):string
+    {
+       
+        $data = date('Y');
+        $aniversario = $data - $dataNasc;
+        
+        return $aniversario;
+    }
 
 }
