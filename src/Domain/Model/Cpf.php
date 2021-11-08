303746ca878eeab3836a0d3ae406fb90a0c4aebb
weliton <welingtonzsilva@gmail.com>
2021-11-08 20:33:22 -0300

    classe restruturada, para ser chamada pelo cliente

--- src/Domain/Model/Cpf.php
+++ src/Domain/Model/Cpf.php
@@ -9,20 +9,16 @@ class Cpf extends Pessoa
     public function __construct(string $cpf)
     {
         
-        if($this->validaCpf($cpf)){
-            $this->setCpf($cpf);
-        }else{
-            $this->setCpf('Cpf Invalido tente xxx.xxx.xxx-xx');
-        }
+        $this->cpf= $cpf;
     }
 
-    private function validaCpf(string $cpf):int
+    public function validaCpf(string $cpf):int
     {
         return preg_match('/^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$/',$cpf,$encontrados);
     }
-
-    private function setCpf(string $cpf):void
+    
+    public function cpf()
     {
-        $this->cpf = $cpf;
+        return $this->cpf;
     }
 }
